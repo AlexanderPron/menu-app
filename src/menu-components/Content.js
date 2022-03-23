c1ce109ee2b1de8646072e066800df96303bb24c
Gadya <zakuson2001@inbox.ru>
2022-03-23 23:42:36 +0300

    ignore warning

--- src/menu-components/Content.js
+++ src/menu-components/Content.js
@@ -5,6 +5,7 @@ export default function Content(props) {
         <div className="content">
             {
                 props.menuItems.map(menuItem => {
+                    // eslint-disable-next-line
                     return <div className={menuItem.target} key = {menuItem.id}> <a name={menuItem.target}> </a> </div>
                 })}
         </div>
