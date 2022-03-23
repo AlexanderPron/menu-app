c14cf29a058f14a7c746d28233c92cb3bdea274d
Gadya <zakuson2001@inbox.ru>
2022-03-23 23:33:22 +0300

    Updates

---
+++ precache-manifest.926ac7745b88d263a6139f64225507ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0bdb8666620d5eb0efa9",
+    "url": "/menu-app/static/css/main.e6eab977.chunk.css"
+  },
+  {
+    "revision": "0bdb8666620d5eb0efa9",
+    "url": "/menu-app/static/js/main.0bdb8666.chunk.js"
+  },
+  {
+    "revision": "4422e644433f4a61be16",
+    "url": "/menu-app/static/js/1.4422e644.chunk.js"
+  },
+  {
+    "revision": "d695bbf511b0d2c16a8a",
+    "url": "/menu-app/static/css/2.897ddaaa.chunk.css"
+  },
+  {
+    "revision": "d695bbf511b0d2c16a8a",
+    "url": "/menu-app/static/js/2.d695bbf5.chunk.js"
+  },
+  {
+    "revision": "c7b05f1df4c8517151ed",
+    "url": "/menu-app/static/js/runtime~main.c7b05f1d.js"
+  },
+  {
+    "revision": "7fbd789912622aedc50e452c43ca94af",
+    "url": "/menu-app/index.html"
+  }
+];
