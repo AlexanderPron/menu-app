b575bf7952982ae9837879dcf428f3f26acf4ea4
Gadya <zakuson2001@inbox.ru>
2022-04-04 22:34:00 +0300

    continue caurusel 2

--- src/menu-components/MenuItem.js
+++ src/menu-components/MenuItem.js
@@ -3,27 +3,26 @@ import { useState } from "react";
 import { Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { LinkContainer } from "react-router-bootstrap";
-import menuItemList from "./Main";
 
 export default function MenuItem(props) {
-  let [isActive, setActiveItem]  = useState(props.menuItem.isActive)
+  // let [isActive, setActiveItem] = useState(props.menuItem.isActive);
   if (props.menuItem.isChapterContent) {
     return (
       <LinkContainer to={`/report/${props.menuItem.target}`}>
-        <Nav.Link onClick={(event) => {
-          event.preventDefault();
-          isActive = true;
-          menuItemList.map((item)=>{
-            if (item.id === props.menuItem.id){
-              item.isActive = true;
-            }
-            else {
-              item.isActive = false;
-            }
-            return item;
-          })
-          setActiveItem(isActive);
-          }}>
+        <Nav.Link
+        // onClick={(event) => {
+        //   event.preventDefault();
+        //   for (let item of menuItemList) {
+        //     if (item.id === props.menuItem.id) {
+        //       item.isActive = true;
+        //     } else {
+        //       item.isActive = false;
+        //     }
+        //   }
+        //   isActive = true;
+        //   setActiveItem(isActive);
+        // }}
+        >
           {props.menuItem.value}
         </Nav.Link>
       </LinkContainer>
